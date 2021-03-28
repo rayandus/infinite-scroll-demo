329488ea29d1b7b2fafbd0743fb66acd49c83080
rayandus <33485319+rayandus@users.noreply.github.com>
2021-03-29 02:52:03 +0800

    Lint fix, remove App.css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import Provider from 'react-redux/es/components/Provider';
 import Layout from './app/components/layout/Layout';
 import store from './store';
-import './App.css';
 
 function App() {
   return (
