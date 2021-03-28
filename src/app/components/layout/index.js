86069433a6a5333c78feaefa84ae185aa6e19334
rayandus <33485319+rayandus@users.noreply.github.com>
2021-03-29 04:33:52 +0800

    Fix folder text-case renaming issue

---
+++ src/app/components/layout/index.js
@@ -0,0 +1,4 @@
+/* eslint-disable import/no-unresolved */
+export { default as Layout } from './Layout';
+export { default as HeaderBar } from './HeaderBar';
+export { default as SideBar } from './SideBar';
