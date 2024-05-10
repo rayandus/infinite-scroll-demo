726257cca631fcc0d996f20bf1725dba65d7724e
Ray Andus <ray.andus@ombori.com>
2024-05-11 03:00:08 +0800

    feat: add delay for testing purposes

--- src/app/components/pokemons/api/PokemonsApi.js
+++ src/app/components/pokemons/api/PokemonsApi.js
@@ -1,6 +1,20 @@
 /* eslint-disable import/no-unresolved */
 import axios from 'axios';
 
+const delay = (delayMs, resolveValue) => {
+  return new Promise(((resolve) => {
+    if (delayMs) {
+      setTimeout(() => {
+        resolve(resolveValue);
+      }, delayMs);
+
+      return;
+    }
+
+    resolve(resolveValue);
+  }));
+};
+
 class PokemonsApi {
     getList = (params) => {
       const apiURL = `${process.env.REACT_APP_POKE_API_URL}pokemon${params}`;
@@ -8,7 +22,7 @@ class PokemonsApi {
       return new Promise((resolve, reject) => {
         axios.get(apiURL).then((response) => {
           const infos = response.data.results.map((result) => {
-            return this.getInfo(result.name);
+            return delay(0, this.getInfo(result.name));
           });
 
           Promise.all(infos).then((data) => {
